32480acef686c139882faf48c23454591d2226e9
pescudie <pescudie@orha.fr>
2023-12-12 00:31:59 +0100

    image

---
+++ tilesets/Logo Elaborha.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Logo Elaborha" tilewidth="30" tileheight="120" tilecount="54" columns="27">
+ <image source="../../../../../Dropbox (ORHA)/Dossier de l'équipe ORHA/2 ORHA/03 COMMUNICATION/00 ELABORHA COMMUNICATION/Logo Elaborha/elaborha horizontal/elaborha R.png" width="815" height="240"/>
+</tileset>
